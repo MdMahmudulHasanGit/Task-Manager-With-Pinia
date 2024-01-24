863752ed2180679283234911bb7bf617f7835686
MdMahmudulHasanGit <mahmudul209307@gmail.com>
2024-01-25 01:16:15 +0600

    Install pinia for state management

---
+++ src/components/AComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>A Component</h2>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/BComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <hr>
+    <h2>B Component</h2>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
